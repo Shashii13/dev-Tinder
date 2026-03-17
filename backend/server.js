de36b3d6bb0d9c9e6c365e3f8703ffefdca78c35
YourGitHubUsername <youremail@example.com>
2026-03-18 00:36:54 +0530

    setup express server with basic middleware

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+  })
+);
+
+
+app.get("/", (req, res) => {
+  res.send("DevTinder API is running 🚀");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
